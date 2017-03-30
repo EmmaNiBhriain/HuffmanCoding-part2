3cbe91ebdce4365ed21c208ae8067724c7f09678
Emma Ni Bhriain <obrienemma0@gmail.com>
2017-03-30 21:07:09 +0100

    Input streams added
    Decoded has been updated to use input streams to read a file instead of
    a file buffer.

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,18 +31,22 @@ public class Decoder {
 		//read each character in a file
 		//split into header, tree, message
 		//call treeBuilder
-		BufferedReader buffer = new BufferedReader(new FileReader("EncodedData.dat"));		
-		int charIndex;// = inDataStream.read();
-		char nextCharacter;
+		File aFile = new File("EncodedData.dat");
+		FileInputStream inByteStream = new FileInputStream(aFile);
+		DataInputStream inDataStream = new DataInputStream(inByteStream);
+		
+		//BufferedReader buffer = new BufferedReader(new FileReader("EncodedData.dat"));		
+		//int charIndex;// = inDataStream.read();
+		char nextCharacter = ' ';
 		char decodedChar;
 		currentNode = loader.getNode1();
 		boolean tree = false;
 		try {
-			while ((charIndex = buffer.read())!= -1) {
-				nextCharacter = (char) charIndex;
+			while (inDataStream.available()>0) {
+				nextCharacter = (char) inDataStream.read();
 				if((nextCharacter == '0')&&(currentNode.getLeftChild()!= null)){
 					currentNode = currentNode.getLeftChild();
-					//System.out.println(currentNode.letter);
+					System.out.println(currentNode.letter);
 				}
 				else if((nextCharacter == '0')&&(currentNode.getLeftChild()== null)){
 					System.out.println(currentNode.letter);
@@ -51,7 +57,7 @@ public class Decoder {
 				
 				else if((nextCharacter == '1')&&(currentNode.getRightChild()!= null)){
 					currentNode = currentNode.getRightChild();
-					//System.out.println(currentNode.letter);
+					System.out.println(currentNode.letter);
 				}
 				else if((nextCharacter == '1')&&(currentNode.getRightChild()== null)){
 					System.out.println(currentNode.letter);
@@ -59,38 +65,7 @@ public class Decoder {
 
 					currentNode = loader.getNode1().getRightChild();
 				}
-				/*else if((currentNode.getRightChild() == null)&&(currentNode.getLeftChild()==null)){
-					System.out.println(currentNode.letter);
-					currentNode = loader.getNode1();
-				}*/
 				
-				
-				/*if()//after the header){
-					//treeBuilder();
-					{Node node = new Node("root",0,null,null);
-					if(nextCharacter == '0');
-						node.setLeftChild(new Node("left", 0, nu));
-				}*/
-				
-				/*if(nextCharacter == '/'){
-					tree = true;
-				}
-				*/
-				//does not reach this code
-				/*while(tree == true){
-					currentNode = loader.getNode1();
-					if (nextCharacter == '0'){
-						currentNode = currentNode.getLeftChild();
-					}
-					else if(nextCharacter == '1'){
-						currentNode = currentNode.getRightChild();
-					}
-					else{
-						System.out.println(nextCharacter);
-						
-					}
-						
-				}*/
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
