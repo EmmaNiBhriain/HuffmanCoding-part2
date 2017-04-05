da1a5e7bd211776ae36706c9e55fc5c1ba054cf2
Emma Ni Bhriain <obrienemma0@gmail.com>
2017-04-05 22:17:19 +0100

    Outputting message in binary
    Writing the ID and the message of the file as binary to a file.

--- src/Loader.java
+++ src/Loader.java
@@ -3,6 +3,8 @@ import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,14 +23,18 @@ public class Loader {
 	private boolean DEBUGMAP = false;
 	private boolean DEBUGQUEUE = false;
 	private boolean DEBUGTREE = false;
-	private boolean DEBUGBINARY = true;
-	private boolean DEBUGENCODE = true;
+	private boolean DEBUGBINARY = false;
+	private boolean DEBUGENCODE = false;
 	private boolean DEBUGHEADER = false;
 	
 	FileWriter filewriter1 = new FileWriter();
 	FileWriter filewriter2 = new FileWriter();
 	FileWriter filewriter3 = new FileWriter();
 	private StringBuffer total = new StringBuffer("");
+	private byte[] binaryID = new byte[4];
+	private byte[] binaryMessage;
+	
+	private StringBuffer encoded = new StringBuffer("");
 	private File file = null;
 	private Node node1 = new Node(null, 0, null, null);
 	
@@ -43,7 +49,12 @@ public class Loader {
 		createTree();
 		total = this.getBinaryValue(node1, total);
 		
-		encodeFile(file);
+		try {
+			encodeFile(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 
@@ -65,7 +76,7 @@ public class Loader {
 		//int fileSize = inByteStream.available();
 		
 		//DataInputStream inDataStream = new DataInputStream(inByteStream);
-		BufferedReader buffer = new BufferedReader(new FileReader("C:\\Users\\eob00\\Desktop\\dataTest.dat"));		
+		BufferedReader buffer = new BufferedReader(new FileReader(file));		
 //		int fileSize = inDataStream.available();
 		int charIndex;// = inDataStream.read();
 
@@ -87,6 +98,7 @@ public class Loader {
 			
 		}
 		//inDataStream.close();
+		System.out.println("length of original file : " + file.length());
 		filewriter1.writeToFile(file, Character.toString(eof));
 		pairMap.put(eof, 1);
 		filewriter1.close1();
@@ -212,19 +224,36 @@ public class Loader {
 	/**
 	 * Write to the encoded file. Include id, header, divider and the binary values for the characters.
 	 * @param file2
+	 * @throws IOException 
 	 */
-	public void encodeFile(File file2) {	
-		String divider = "/";
-		String id = "Emma";
+	public void encodeFile(File file2) throws IOException {	
 		
-		//filewriter2.write(id);
+		File bFile = new File("Encodedbyte.dat");
+		FileOutputStream outByteStream = new FileOutputStream(bFile);
+		String divider = "/";
+		String id = "00001100101011011101000010011001";
+		/*String temp;
+		byte temp1;
+		temp = id.substring(0,8);
+		temp1 = Byte.parseByte(temp, 2);
+		System.out.println(temp1);
+		binaryID[0] = temp1;
+		outByteStream.write(binaryID[0]);*/
+		//System.out.println("size: "+ bFile.length());
+		//add id to the stringBuffer representing the binary for the file
+		encoded.append(id);
+		System.out.println("id = " + encoded.toString());
+		//stringToBinary(encoded);
+		
+		
 		StringBuffer tempHeader = new StringBuffer("");
 		StringBuffer header = buildHeader(node1, tempHeader);
-		
+		System.out.println("header = " + header.toString());
+		//write out header as plain text 
 		if(DEBUGHEADER == true) System.out.println(header.toString());
 		filewriter2.write(header.toString());
 		
-		filewriter2.write(divider);
+		filewriter2.write(divider); //write out divider as plain text too
 		
 		//Write the character code for each letter
 		int charCount = 0;
@@ -236,7 +265,8 @@ public class Loader {
 				nextCharacter = Character.toString((char) charCount);
 				if (countMap.containsKey(nextCharacter)) {
 					replacedChar = countMap.get(nextCharacter);
-					filewriter2.write(replacedChar);
+					encoded.append(replacedChar);
+					
 					if (DEBUGENCODE == true)
 						System.out.println(replacedChar);
 				} else {
@@ -247,7 +277,15 @@ public class Loader {
 			System.out.println("Error retrieving huffman");
 		}
 		
-		filewriter2.close();
+		
+		System.out.println(encoded.toString());
+		stringToBinary(encoded);
+		outByteStream.write(binaryID);
+		outByteStream.write(binaryMessage);
+		outByteStream.close();
+		System.out.println("size of encoded message: " + bFile.length());
+		//filewriter2.write(encoded.toString());
+		//filewriter2.close();
 		//node1 = Node.clearTree(node1);
 	}
 
@@ -280,6 +318,37 @@ public class Loader {
 		}
 		
 		return tree;
+	}
+	
+	/**
+	 * Convert the encoded data from a string of 1s and 0s to binary bits
+	 * Important to note: range of a byte is -128 to 127 so make into an int instead
+	 * @param encoded - the string to be converted to binary
+	 */
+	public void stringToBinary(StringBuffer encoded){
+		String temp;
+		byte temp1;
+		int sizeArray = encoded.length()/8+1;
+		binaryMessage = new byte[sizeArray];
+		int i = 0;
+		while(encoded.length() >= 8){
+			
+			temp = encoded.substring(0, 8);
+			System.out.println(temp);
+			temp1 = (byte) Integer.parseInt(temp, 2);
+			binaryMessage[i] = temp1;
+			System.out.println(temp1);
+			encoded.delete(0, 8);
+			i++;
+		}
+		while((encoded.length() < 8)&&(encoded.length()>0)){
+			encoded.append("0");			//TO DO: LOOK INTO BIT STUFFING
+		}
+		temp = encoded.toString();
+		System.out.println(temp);
+		temp1 = (byte) Integer.parseInt(temp, 2);
+		binaryMessage[i] = temp1;
+		System.out.println(temp1);
 	}
 
 
